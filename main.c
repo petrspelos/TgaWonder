ceb7b7c22a164cc9a021e13e45df5ce4cc46f5bd
petrspelos <petr.spelos@gmail.com>
2019-03-18 08:58:19 +0100

    Fix horizontal flip and implement rect

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct color
+typedef struct color
 {
     unsigned char r, g, b;
 };
@@ -9,6 +9,13 @@ struct color
 void addTgaHeader(FILE *ft, int width, int height);
 
 void commitImage(FILE *ft, struct color *data, int width, int height);
+
+void rect(color *data, int imgWidth, int imgHeight, int x, int y, int width, int height, color *fill);
+
+int coordToOffset(int x, int y, int width, int height)
+{
+    return x * width + y;
+}
 
 int main(void)
 {
@@ -20,16 +27,31 @@ int main(void)
     ft = fopen(name, "w");
     addTgaHeader(ft, width, height);
 
-    struct color *ptr;
-    ptr = (struct color*) malloc((width * height) * sizeof(struct color));
+    color *ptr;
+    ptr = (color*) malloc((width * height) * sizeof(color));
 
     int resolution = width * height;
-    for(int i = 0; i < resolution; i++)
+    for(int i = 0; i <= resolution; i++)
     {
         (ptr + i)->r = 255;
         (ptr + i)->g = 0;
-        (ptr + i)->b = 0;
+        (ptr + i)->b = 255;
     }
+
+    color *white;
+    white = (color*) malloc(sizeof(color));
+    white->r = 255;
+    white->g = 255;
+    white->b = 255;
+
+    color *red;
+    red = (color*) malloc(sizeof(color));
+    red->r = 255;
+    red->g = 0;
+    red->b = 0;
+
+    rect(ptr, width, height, 50, 50, 50, 50, white);
+    rect(ptr, width, height, 0, 49, 2, 2, red);
 
     commitImage(ft, ptr, width, height);
 
@@ -58,11 +80,33 @@ void addTgaHeader(FILE *ft, int width, int height)
 
 void commitImage(FILE *ft, struct color *data, int width, int height)
 {
-    for(int i = 0; i < width * height; i++)
+    for(int y = height - 1; y >= 0; y--)
     {
-        putc((data + i)->b, ft);
-        putc((data + i)->g, ft);
-        putc((data + i)->r, ft);
+        for(int x = 0; x < width; x++)
+        {
+            putc((data + coordToOffset(x, y, width, height))->b, ft);
+            putc((data + coordToOffset(x, y, width, height))->g, ft);
+            putc((data + coordToOffset(x, y, width, height))->r, ft);
+        }
+    }
+    //for(int i = 0; i < width * height; i++)
+    //{
+        //putc((data + i)->b, ft);
+        //putc((data + i)->g, ft);
+        //putc((data + i)->r, ft);
+    //}
+}
+
+void rect(color *data, int imgWidth, int imgHeight, int x, int y, int width, int height, color *fill)
+{
+    for(int dx = x; dx < x + width; dx++)
+    {
+        for(int dy = y; dy < y + height; dy++)
+        {
+            (data + coordToOffset(dx, dy, imgWidth, imgHeight))->r = fill->r;
+            (data + coordToOffset(dx, dy, imgWidth, imgHeight))->g = fill->g;
+            (data + coordToOffset(dx, dy, imgWidth, imgHeight))->b = fill->b;
+        }
     }
 }
 
